38a8e4b88547cad403cbdd3e56976550fee0e070
Valery Filipyev <valeranuzhdin1998@mail.ru>
2018-12-07 16:07:22 +0300

    styling and construct the form for order

---
+++ src/components/UI/Input/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './index.css';
+
+const input = (props) => {
+  let inputElement = null;
+  
+  switch (props.inputtype) {
+    case ('input'):
+      inputElement = <input className={classes.InputElement} {...props}/>;
+      break;
+    case ('textarea'):
+      inputElement = <textarea className={classes.InputElement} {...props}/>;
+      break;
+    default:
+      inputElement = <input className={classes.InputElement} {...props}/>
+  }
+  
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  )
+};
+
+input.proptypes = {
+  inputtype: PropTypes.string.isRequired,
+  inputElement: PropTypes.object.isRequired
+};
+
+export default input;
