0ca4ea83407e3119ccc2356a43b4090a9e0abf21
Valery Filipyev <valeranuzhdin1998@mail.ru>
2018-12-22 17:22:27 +0300

    prepare the folder structure

--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary';
 import Spinner from '../../components/UI/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actionTypes';
 
 class BurgerBuilder extends Component {
   constructor(props) {
