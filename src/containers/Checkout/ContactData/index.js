ba071d8055e160ef242cf2ce59a2fede68f632af
Valery Filipyev <valeranuzhdin1998@mail.ru>
2018-12-27 18:33:06 +0300

    order actions

--- src/containers/Checkout/ContactData/index.js
+++ src/containers/Checkout/ContactData/index.js
@@ -98,8 +98,7 @@ class ContactData extends Component {
           valid: true
         }
       },
-      formIsValid: false,
-      loading: false
+      formIsValid: false
     }
   }
   
@@ -201,7 +200,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner/>;
     }
     return (
@@ -216,12 +215,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  };
 };
 
 ContactData.propTypes = {
@@ -238,4 +240,4 @@ ContactData.propTypes = {
   inputChangedHandler: PropTypes.func
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
