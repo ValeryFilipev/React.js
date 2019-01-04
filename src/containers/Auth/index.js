3030ec2b487470215a42a9477a0095332229e47f
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-01-04 18:09:37 +0300

    redirect to the checkout page

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -48,6 +48,12 @@ class Auth extends Component {
     }
   }
   
+  componentDidMount() {
+    if (!this.props.buildingBurger && this.props.auth !== '/') {
+      this.props.onSetAuthRedirectPath();
+    }
+  }
+  
   checkValidity = (value, rules) => {
     let isValid = true;
     
@@ -145,7 +151,7 @@ class Auth extends Component {
     
     let authRedirect = null;
     if (this.props,isAuthenticated) {
-      authRedirect = <Redirect to="/"/>
+      authRedirect = <Redirect to={this.props.authRedirectPath}/>
     }
     
     return (
@@ -167,13 +173,16 @@ const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
   }
 };
 
