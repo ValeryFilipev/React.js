54bf968735f42dd3d5bdf9d3ef7f3c2b154ae94c
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-03-05 16:32:01 +0300

    move orders-logic into saga

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,18 +1,17 @@
 import * as actionTypes from "./actionTypes";
-import axios from "../../axios-orders";
 
 export const purchaseBurgerSuccess = (id, orderData) => {
   return {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
     orderId: id,
-    orderData: orderData
+    orderData
   };
 };
 
 export const purchaseBurgerFail = error => {
   return {
     type: actionTypes.PURCHASE_BURGER_FAIL,
-    error: error
+    error
   };
 };
 
@@ -23,17 +22,11 @@ export const purchaseBurgerStart = () => {
 };
 
 export const purchaseBurger = (orderData, token) => {
-  return dispatch => {
-    dispatch(purchaseBurgerStart());
-    axios
-      .post("/orders.json?auth=" + token, orderData)
-      .then(response => {
-        dispatch(purchaseBurgerSuccess(response.data.name, orderData));
-      })
-      .catch(error => {
-        dispatch(purchaseBurgerFail(error));
-      });
-  };
+  return {
+    type: actionTypes.PURCHASE_BURGER,
+    orderData,
+    token
+  }
 };
 
 export const purchaseInit = () => {
@@ -45,14 +38,14 @@ export const purchaseInit = () => {
 export const fetchOrdersSuccess = orders => {
   return {
     type: actionTypes.FETCH_ORDERS_SUCCESS,
-    orders: orders
+    orders
   };
 };
 
 export const fetchOrdersFail = error => {
   return {
     type: actionTypes.FETCH_ORDERS_FAIL,
-    error: error
+    error
   };
 };
 
@@ -63,26 +56,9 @@ export const fetchOrdersStart = () => {
 };
 
 export const fetchOrders = (token, userId) => {
-  return dispatch => {
-    dispatch(fetchOrdersStart());
-    const queryParams =
-      "?auth=" + token + '&orderBy="userId"&equalTo="' + userId + '"';
-    axios
-      .get("/orders.json" + queryParams)
-      .then(res => {
-        const fetchedOrders = [];
-        for (let key in res.data) {
-          if (res.data.hasOwnProperty(key)) {
-            fetchedOrders.push({
-              ...res.data[key],
-              id: key
-            });
-          }
-        }
-        dispatch(fetchOrdersSuccess(fetchedOrders));
-      })
-      .catch(err => {
-        dispatch(fetchOrdersFail(err));
-      });
-  };
+  return {
+    type: actionTypes.FETCH_ORDERS,
+    token,
+    userId
+  }
 };
