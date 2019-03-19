0417b88a7a0e97909eaa5ec42cfcbb46a50779e5
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-03-19 13:59:16 +0300

    inject hooks into project
    Update the version of React and make it possible to use hooks in the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import asyncComponent from "./hoc/asyncComponent";
@@ -20,12 +20,12 @@ const asyncAuth = asyncComponent(() => {
   return import("./containers/Auth");
 });
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
+const app = props => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
 
-  render() {
+  
     let routes = (
       <Switch>
         <Route path="/auth" component={asyncAuth} />
@@ -34,7 +34,7 @@ class App extends Component {
       </Switch>
     );
 
-    if (this.props.isAuthenticated) {
+    if (props.isAuthenticated) {
       routes = (
         <Switch>
           <Route path="/auth" component={asyncAuth} />
@@ -53,8 +53,7 @@ class App extends Component {
         </Layout>
       </div>
     );
-  }
-}
+};
 
 const mapStateToProps = state => {
   return {
@@ -72,5 +71,5 @@ export default withRouter(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )(App)
+  )(app)
 );
